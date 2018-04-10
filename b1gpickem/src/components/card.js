7991f8748cdaa5d18647ec9070084e75a2dfc2a5
Troy Crawford <Troy@lechler.de>
2018-04-10 07:13:13 -0500

    cleanup

--- b1gpickem/src/components/card.js
+++ b1gpickem/src/components/card.js
@@ -4,7 +4,7 @@ const Card = (props) => {
     return (
         //use onClick function to call parent method and then pass through the id of the clicked item for funtion
     <a className="cardLink" onClick={() => props.checkSchool(props.id)}>    
-        <div className="col-md-3 col-lg-3 schoolCard text-center">
+        <div className=" col-sm-6 col-md-3 col-lg-3 col-xl-3 schoolCard text-center">
             <img className="cardImg" src={props.image} alt={props.name} key={props.id}/>
         </div>
     </a>
